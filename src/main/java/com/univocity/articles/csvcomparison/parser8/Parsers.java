f2cb3e19f782659dbd76cf9a715fa2419ec4459a
Arne Burmeister <arne.burmeister@tui.com>
2015-10-01 14:28:05 +0200

    return unmodifiable list

--- src/main/java/com/univocity/articles/csvcomparison/parser8/Parsers.java
+++ src/main/java/com/univocity/articles/csvcomparison/parser8/Parsers.java
@@ -3,15 +3,22 @@ package com.univocity.articles.csvcomparison.parser8;
 import com.univocity.articles.csvcomparison.parser.AbstractParser;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @SuppressWarnings("unused")
 public class Parsers {
+    
+    private static final List<AbstractParser> JAVA8_PARSERS = asList(
+        new DecsParser()
+    );
 
     public static List<AbstractParser> list() {
         List<AbstractParser> allParsers = new ArrayList<>(com.univocity.articles.csvcomparison.parser.Parsers.list());
-        allParsers.add(new DecsParser());
-        return allParsers;
+        allParsers.addAll(JAVA8_PARSERS);
+        return Collections.unmodifiableList(allParsers);
     }
 
 }
